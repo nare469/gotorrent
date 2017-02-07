51ea889ba0e11556d41412b6829afbf8e02191b8
Narendran Srinivasan <nare469@gmail.com>
2017-02-06 20:12:27 -0500

    Connection stuff

--- connection/connection.go
+++ connection/connection.go
@@ -2,18 +2,32 @@ package connection
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"github.com/nare469/gotorrent/parser"
+	"math/rand"
+	"net"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 )
 
-func Connect(attrs parser.TorrentAttrs) {
+func Connect(attrs parser.TorrentAttrs) (x parser.TrackerAttrs, err error) {
+	u, err := url.Parse(attrs.Announce)
+	if u.Scheme == "udp" {
+		x, err = connectUdp(attrs)
+	} else {
+		x, err = connectHttp(attrs)
+	}
+	return
+}
+
+func connectHttp(attrs parser.TorrentAttrs) (x parser.TrackerAttrs, err error) {
 	req, err := http.NewRequest("GET", attrs.Announce, nil)
 
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	q := req.URL.Query()
@@ -27,32 +41,81 @@ func Connect(attrs parser.TorrentAttrs) {
 	len, err := attrs.Length()
 
 	if err != nil {
-		panic(err)
+		return
 	}
 	q.Add("left", strconv.FormatInt(len, 10))
 	req.URL.RawQuery = q.Encode()
 
-	fmt.Println(req.URL.String())
-
 	resp, err := http.Get(req.URL.String())
 
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	defer resp.Body.Close()
-	fmt.Println(resp)
 
 	// TODO: Remove logging of the string sometime soon
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 	s := buf.String()
 
-	fmt.Println(s)
+	x, err = parser.NewTrackerAttrs(strings.NewReader(s))
 
-	_, err = parser.NewTrackerAttrs(strings.NewReader(s))
+	return
+
+}
+
+func connectUdp(attrs parser.TorrentAttrs) (x parser.TrackerAttrs, err error) {
+	u, err := url.Parse(attrs.Announce)
+	addr, err := net.ResolveUDPAddr("udp", u.Host)
+	if err != nil {
+		return
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	// TODO: Retries
+	packet := new(bytes.Buffer)
+	var protocolId uint64 = 0x41727101980
+	var action uint32 = 0
+	transactionId := rand.Uint32()
+
+	binary.Write(packet, binary.BigEndian, protocolId)
+	binary.Write(packet, binary.BigEndian, action)
+	binary.Write(packet, binary.BigEndian, transactionId)
+	_, err = conn.Write(packet.Bytes())
+	if err != nil {
+		return
+	}
+
+	response := make([]byte, 16)
+
+	_, err = conn.Read(response)
+
+	responseBuffer := bytes.NewBuffer(response)
+
+	var actionResponse uint32
+	var transactionResponse uint32
+	var connectionId uint64
+
+	err = binary.Read(responseBuffer, binary.BigEndian, &actionResponse)
+	err = binary.Read(responseBuffer, binary.BigEndian, &transactionResponse)
+	err = binary.Read(responseBuffer, binary.BigEndian, &connectionId)
 
 	if err != nil {
-		panic(err)
+		return
 	}
+
+	announceRequest := new(bytes.Buffer)
+
+	return
+
+}
+
+func populateUDPAnnounce(buf *bytes.Buffer) {
+	// TODO: this
 }
